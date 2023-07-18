8c0850773396e9d9a66a70ec316376711e07abd3
Moses <mochengo29@gmail.com>
2023-07-18 13:28:44 +0300

    Completed assingment

--- src/data/user.js
+++ src/data/user.js
@@ -1,7 +1,7 @@
 const user = {
-  name: "Liza",
-  city: "New York",
-  bio: "I made this!",
+  name: "Moses",
+  city: "Nairobi",
+  bio: "I made this",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
